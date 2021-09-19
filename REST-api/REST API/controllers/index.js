520eb9793258f70dc2cd78c48a340753ffb8bd44
dilyana2001 <dilyana2001@gmail.com>
2021-09-19 21:54:11 +0300

    clear some files

--- REST-api/REST API/controllers/index.js
+++
@@ -1,7 +0,0 @@
-const user = require('./user');
-const origami = require('./origami');
-
-module.exports = {
-    user,
-    origami
-};
--- REST-api/REST API/models/index.js
+++
@@ -1,9 +0,0 @@
-const User = require('./User');
-const Origami = require('./Origami');
-const TokenBlacklist = require('./TokenBlacklist');
-
-module.exports = {
-    User,
-    Origami,
-    TokenBlacklist
-};
